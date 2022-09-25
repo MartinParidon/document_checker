8b2d3aa2a1d9652a441fde2be1af119801d179f2
MartinParidon <martinparidon@gmail.com>
2022-09-25 17:55:35 +0200

    Removed bat

--- call_plagiariasm_checker_find.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set /p doc_path=Enter Link to folder of docs  
-set /p out_path=Enter Link to output folder  
-
-python scripts/plagiarism_checker.py %doc_path%  %out_path% find
-
-pause
